3e0df4184e8ac8a832b8ac12161a4ad68842e0cb
Предраг Николић <idmpepe@gmail.com>
2021-09-08 23:04:45 +0200

    Find the project ts server path, else fallback to bundled ts server path (#22)
    * Find the project ts server path, else fallback to bundled ts server path
    
    * same but simpler
    
    Co-authored-by: Rafał Chłodnicki <rchl2k@gmail.com>
    
    * use cls._node_bin() instead of 'node'
    
    * use subprocess.check_output
    
    * use universal_newlines=True instead of decoding
    
    * ensure newline at eof
    
    * keep line length at 120 max
    
    * keep typescript.serverPath in the settings to allow the user to override it
    
    Co-authored-by: Predrag Nikolic <mail@predrag.pro>
    Co-authored-by: Rafał Chłodnicki <rchl2k@gmail.com>

--- plugin.py
+++ plugin.py
@@ -1,5 +1,10 @@
+from LSP.plugin import ClientConfig
+from LSP.plugin import WorkspaceFolder
+from LSP.plugin.core.typing import List, Optional
 from lsp_utils import NpmClientHandler
 import os
+import sublime
+import subprocess
 
 
 def plugin_loaded():
@@ -14,3 +19,23 @@ class LspVolarPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'server'
     server_binary_path = os.path.join(server_directory, 'node_modules', '@volar', 'server', 'out', 'index.js')
+
+    @classmethod
+    def is_allowed_to_start(
+        cls,
+        window: sublime.Window,
+        initiating_view: Optional[sublime.View] = None,
+        workspace_folders: Optional[List[WorkspaceFolder]] = None,
+        configuration: Optional[ClientConfig] = None
+    ):
+        if not workspace_folders or not configuration:
+            return
+        if configuration.init_options.get('typescript.serverPath'):
+            return # don't find the `typescript.serverPath` if it was set explicitly in LSP-volar.sublime-settings
+        server_directory_path = cls._server_directory_path()
+        resolve_module_script = os.path.join(server_directory_path, 'resolve_module.js')
+        first_folder = workspace_folders[0].path
+        command =  [cls._node_bin(), resolve_module_script, first_folder, 'typescript/lib/tsserverlibrary.js']
+        workspace_ts_path = subprocess.check_output(command, universal_newlines=True)
+        bundled_ts_path = os.path.join(server_directory_path, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js')
+        configuration.init_options.set('typescript.serverPath', workspace_ts_path or bundled_ts_path)
