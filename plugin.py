9cc8db3fec58146c68064c142836b46894181d4a
Предраг Николић <idmpepe@gmail.com>
2022-04-27 12:44:46 -0700

    Update JSON schema  (#77)
    * update json schema
    
    Some settings have been skipped, because they are VS Code related, or they are simply not supported:
    "volar-language-features.trace.server" - Traces the communication between VS Code and the language server.
    "volar-language-features-2.trace.server" - Traces the communication between VS Code and the language server.
    "volar-document-features.trace.server" - Traces the communication between VS Code and the language server.
    "volar.vueserver.useSecondServer" - Not supported
    "volar.icon.splitEditors" - Not supported - Show split editor icon in title area of editor.
    "volar.icon.preview" - Not supported - Show Vite / Nuxt App preview icon.
    "volar.takeOverMode.enabled" - Skipped, because LSP-volar does it this way https://github.com/sublimelsp/LSP-volar#enable-for-non-vue-files
    "volar.preview.port" - Not supported, yet
    "volar.preview.backgroundColor" - Not supported
    "volar.preview.transparentGrid" - Not supported
    
    DEPRECATIONS
    
    "volar.preferredAttrNameCase" was renamed to "volar.completion.preferredAttrNameCase"
    "volar.preferredTagNameCase" was renamed to "volar.completion.preferredTagNameCase"
    
    * LSP supports implementation
    
    https://github.com/johnsoncodehk/volar/blob/master/CHANGELOG.md#0330-2022313
    > eat: support for goto implementations
        if you're not using VSCode, you should config new option languageFeatures.implementation = true in initializationOptions to enable it
    
    * LSP doesn't supports inlay hints just yet
    
    * refactor: define a find_typescript_path method
    
    * programmatically set initialization options for languageFeatures and documentFeatures

--- plugin.py
+++ plugin.py
@@ -30,12 +30,51 @@ class LspVolarPlugin(NpmClientHandler):
     ):
         if not workspace_folders or not configuration:
             return
+        configuration.init_options.set('languageFeatures', {
+            "references": True,
+            "definition": True,
+            "implementation": True,
+            "typeDefinition": True,
+            "callHierarchy": False,
+            "inlayHints": False,
+            "hover": True,
+            "rename": True,
+            "renameFileRefactoring": False,
+            "signatureHelp": True,
+            "codeAction": True,
+            "completion": {
+                "defaultTagNameCase": get_default_tag_name_case(configuration),
+                "defaultAttrNameCase": get_default_attr_name_case(configuration),
+                "getDocumentNameCasesRequest": False,
+                "getDocumentSelectionRequest": False,
+            },
+            "documentHighlight": True,
+            "documentLink": False,
+            "codeLens": False,
+            "semanticTokens": False,
+            "schemaRequestService": False,
+            "diagnostics": True
+        })
+        configuration.init_options.set('documentFeatures', {
+            "selectionRange": True,
+            "foldingRange": True,
+            "linkedEditingRange": False,
+            "documentSymbol": True,
+            "documentColor": True,
+            "documentFormatting": {
+                "defaultPrintWidth": 90
+            }
+        })
         if configuration.init_options.get('typescript.serverPath'):
             return # don't find the `typescript.serverPath` if it was set explicitly in LSP-volar.sublime-settings
+        typescript_path = LspVolarPlugin.find_typescript_path(workspace_folders[0].path)
+        configuration.init_options.set('typescript.serverPath', typescript_path)
+
+    @classmethod
+    def find_typescript_path(cls, current_folder: str) -> str:
         server_directory_path = cls._server_directory_path()
         resolve_module_script = os.path.join(server_directory_path, 'resolve_module.js')
-        first_folder = workspace_folders[0].path
-        find_ts_server_command =  [cls._node_bin(), resolve_module_script, first_folder]
+        find_ts_server_command =  [cls._node_bin(), resolve_module_script, current_folder]
         startupinfo = None
         # Prevent cmd.exe popup on Windows.
         if sublime.platform() == "windows":
@@ -45,4 +84,20 @@ class LspVolarPlugin(NpmClientHandler):
             )
         workspace_ts_path = subprocess.check_output(find_ts_server_command, universal_newlines=True, startupinfo=startupinfo)
         bundled_ts_path = os.path.join(server_directory_path, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js')
-        configuration.init_options.set('typescript.serverPath', workspace_ts_path or bundled_ts_path)
+        return workspace_ts_path or bundled_ts_path
+
+
+def get_default_tag_name_case(configuration: ClientConfig) -> str:
+    preferred_tag_name_case = configuration.settings.get('volar.completion.preferredTagNameCase')
+    if preferred_tag_name_case == 'kebab':
+        return 'kebabCase'
+    elif preferred_tag_name_case == 'pascal':
+        return 'pascalCase'
+    return 'both'
+
+
+def get_default_attr_name_case(configuration: ClientConfig) -> str:
+    preferred_attr_name_case = configuration.settings.get('volar.completion.preferredAttrNameCase')
+    if preferred_attr_name_case == 'camel':
+        return 'camelCase'
+    return 'kebabCase'
