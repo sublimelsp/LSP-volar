200b8d6bf1175c566434fb01cf9967a3478012d5
Предраг Николић <idmpepe@gmail.com>
2022-09-05 12:34:58 +0200

    Bump server (#122)
    * bump server
    
    * remove experimentalImplicitWrapComponentOptionsWithDefineComponent and experimentalImplicitWrapComponentOptionsWithVue2Extend
    in favor of experimentalComponentOptionsWrapper and experimentalComponentOptionsWrapperEnable
    
    * add volar.vueserver.textDocumentSync client glue
    
    * add initialization options properties to sublime-packages.json
    
    * add client volar.ignoreTriggerCharacters setting
    
    ref https://github.com/sublimelsp/LSP-volar/issues/114#issuecomment-1236423114
    
    * volar.vueserver.textDocumentSync order

--- plugin.py
+++ plugin.py
@@ -1,5 +1,6 @@
 from LSP.plugin import ClientConfig
 from LSP.plugin import WorkspaceFolder
+from LSP.plugin.core.protocol import TextDocumentSyncKindIncremental, TextDocumentSyncKindFull, TextDocumentSyncKindNone
 from LSP.plugin.core.typing import List, Optional
 from lsp_utils import NpmClientHandler
 import os
@@ -30,6 +31,7 @@ class LspVolarPlugin(NpmClientHandler):
     ):
         if not workspace_folders or not configuration:
             return
+        configuration.init_options.set('textDocumentSync', get_text_document_sync(configuration))
         configuration.init_options.set('languageFeatures', get_language_features(configuration))
         configuration.init_options.set('documentFeatures', {
             "selectionRange": True,
@@ -79,6 +81,17 @@ def get_default_attr_name_case(configuration: ClientConfig) -> str:
     return 'kebabCase'
 
 
+def get_text_document_sync(configuration: ClientConfig) -> int:
+    text_document_sync = configuration.settings.get('volar.vueserver.textDocumentSync')
+    if text_document_sync == 'full':
+        return TextDocumentSyncKindFull
+    if text_document_sync == 'none':
+        return TextDocumentSyncKindNone
+    return TextDocumentSyncKindIncremental
+
+def get_ignored_trigger_characters(configuration: ClientConfig) -> str:
+    return configuration.settings.get('volar.ignoreTriggerCharacters') or ""
+
 def get_language_features(configuration: ClientConfig) -> dict:
     language_features = {
         "references": True,
@@ -97,6 +110,7 @@ def get_language_features(configuration: ClientConfig) -> dict:
             "defaultAttrNameCase": get_default_attr_name_case(configuration),
             "getDocumentNameCasesRequest": False,
             "getDocumentSelectionRequest": False,
+            "ignoreTriggerCharacters": get_ignored_trigger_characters(configuration)
         },
         "schemaRequestService": False,
         "documentHighlight": True,
