85f3fa8b9e557e5fe7ffbdbd970148815799bd71
Rafal Chlodnicki <rchl2k@gmail.com>
2023-12-02 23:59:33 +0100

    final ST3 fix (hopefully)

--- plugin.py
+++ plugin.py
@@ -1,8 +1,6 @@
 from LSP.plugin import ClientConfig
 from LSP.plugin import WorkspaceFolder
-from LSP.plugin.core.typing import Any, Callable, List, Optional, Mapping
-from LSP.plugin.core.protocol import Location
-from LSP.plugin.locationpicker import LocationPicker
+from LSP.plugin.core.typing import List, Optional
 from lsp_utils import NpmClientHandler
 import os
 import sublime
@@ -55,32 +53,3 @@ class LspVolarPlugin(NpmClientHandler):
                 return os.path.dirname(candidate)
         server_directory_path = cls._server_directory_path()
         return os.path.join(server_directory_path, 'node_modules', 'typescript', 'lib')
-
-    def on_pre_server_command(self, command: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
-        command_name = command['command']
-        if command_name == 'editor.action.showReferences':
-            _, __, references = command['arguments']
-            self._handle_show_references(references)
-            done_callback()
-            return True
-        return False
-
-    def _handle_show_references(self, references: List[Location]) -> None:
-        session = self.weaksession()
-        if not session:
-            return
-        view = sublime.active_window().active_view()
-        if not view:
-            return
-        if len(references) == 1:
-            args = {
-                'location': references[0],
-                'session_name': session.config.name,
-            }
-            window = view.window()
-            if window:
-                window.run_command('lsp_open_location', args)
-        elif references:
-            LocationPicker(view, session, references, side_by_side=False)
-        else:
-            sublime.status_message('No references found')
